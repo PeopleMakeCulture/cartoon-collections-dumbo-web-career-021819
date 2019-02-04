4d89fb8011cc87b1bf99b5f7bf757253080cc01d
Jing Cao <jingcao.yale@gmail.com>
2019-02-04 15:52:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(array)
   array.map {|a| a.capitalize << "!"}
 end
 
-def long_planeteer_calls(array)
+def long_planeteer_calls1(array)
   array.each do |element|
     if element.length > 4
       return true
@@ -16,6 +16,11 @@ def long_planeteer_calls(array)
   end
   return false
 end
+
+def long_planeteer_calls(array)
+  planeteer_calls.any? { |word| word.length > 4 }
+end
+
 
 
 def find_the_cheese(foods)
